31ce1608ba866b67f534b1493eef0ce102b80e4f
Simon Morgan <sjm@spamcop.net>
2009-07-14 19:58:54 +0100

    Initial import.

---
+++ KewlDownload.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace KewlDownloader
+{
+    partial class KewlDownload
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.filenameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressBar.Location = new System.Drawing.Point(6, 19);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(188, 23);
+            this.progressBar.TabIndex = 11;
+            // 
+            // filenameLabel
+            // 
+            this.filenameLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.filenameLabel.Location = new System.Drawing.Point(6, 6);
+            this.filenameLabel.Name = "filenameLabel";
+            this.filenameLabel.Size = new System.Drawing.Size(188, 13);
+            this.filenameLabel.TabIndex = 12;
+            this.filenameLabel.Text = "<None>";
+            // 
+            // KewlDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.filenameLabel);
+            this.Name = "KewlDownload";
+            this.Padding = new System.Windows.Forms.Padding(6);
+            this.Size = new System.Drawing.Size(200, 152);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label filenameLabel;
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ KewlDownload.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace KewlDownloader
+{
+    public partial class KewlDownload : UserControl
+    {
+        private readonly string filename;
+        private readonly string fullPath;
+        private readonly Uri url;
+
+        private WebClient client;
+
+        public KewlDownload(Uri url, string destDir)
+        {
+            InitializeComponent();
+
+            this.url = url;
+
+            client = new WebClient();
+            client.DownloadProgressChanged += client_DownloadProgressChanged;
+            client.DownloadFileCompleted += client_DownloadFileCompleted;
+
+            filename = Path.GetFileName(url.LocalPath);
+            fullPath = destDir + Path.DirectorySeparatorChar + filename;
+            filenameLabel.Text = filename;
+        }
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            BackColor = Color.LightGreen;
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        public void Start()
+        {
+            client.DownloadFileAsync(url, fullPath);
+        }
+    }
+}
